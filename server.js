875dcfd26cb79d6ecb0724e8b62d5c2214ac0362
Suyash Gupta <suyashgupta2411@gmail.com>
2026-02-10 18:47:35 +0530

    Changed port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/health", (req, res) => res.status(200).send("ok"));
-app.get("/api/hello", (req, res) =>
-  res.json({ message: "hello from bySuyash" }),
-);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`listening on ${port}`));
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
